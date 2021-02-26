37160d9fb17ae29a0854824544f5f1ea15575baa
Ceasar Cyrillus <ceasarcyrillus@businesspartner.roche.com>
2021-02-26 19:29:03 +0100

    Update prettier

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+const PORT = 3001;
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
   await app.enableCors();
-  await app.listen(3001);
-}
-bootstrap();
+  await app.listen(PORT);
+};
+
+bootstrap().then(() => {
+  console.log(`server running on http://localhost:${PORT}`);
+});
